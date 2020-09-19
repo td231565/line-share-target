a6ad1191362d23e2ebbc573188270f409f148044
Richard <td231565@gmail.com>
2020-09-19 09:53:19 +0800

    test msg content

--- src/components/FlexTemplate.vue
+++ src/components/FlexTemplate.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <label>標題</label>
-    <input type="text" v-model="msgObj.altText" />
-    <div v-for="(item, idx) in msgObj.contents" :key="`item-${idx}`"
+    <input type="text" v-model="msgData.altText" />
+    <div v-for="(item, idx) in msgData.contents" :key="`item-${idx}`"
       :style="`background-image: url(${item.url})`"></div>
     <button @click="sendMessageToFriend">Send Message!</button>
   </div>
@@ -10,7 +10,6 @@
 
 <script>
 import liff from '@line/liff'
-import messageTemplate from './msg'
 
 export default {
   name: 'FlexTemplate',
@@ -20,7 +19,7 @@ export default {
   data () {
     return {
       msgLimit: 5, // 輪播式訊息的內容數量上限
-      msgObj: {
+      msgData: {
         altText: '本季最新優惠',
         contents: [
           {
@@ -40,17 +39,49 @@ export default {
     }
   },
   computed: {
-
+    resultMsgTemplate () {
+      let msg = {
+        type: 'flex',
+        altText: this.msgData.altText
+      }
+      msg.contents = this.msgData.contents.map(item => {
+        let contentMsg = {}
+        if (item.type === 'image') {
+          // 產生圖片 bubble
+          contentMsg = {
+            type: 'bubble',
+            body: {
+              type: 'box',
+              layout: 'vertical',
+              contents: [
+                {
+                  type: 'image',
+                  url: item.url,
+                  size: 'full',
+                  aspectMode: 'cover',
+                  gravity: 'top'
+                }
+              ],
+              paddingAll: '0px'
+            }
+          }
+        } else {
+          // 產生結尾 bubble
+        }
+        return contentMsg
+      })
+      return msg
+    }
   },
   methods: {
     // 對好友及群組發送訊息
     sendMessageToFriend () {
       // 需傳入陣列，最多 5 個子物件，每一物件即為一則 message
-      liff.shareTargetPicker([messageTemplate])
+      liff.shareTargetPicker([this.resultMsgTemplate])
     },
     // 轉成純文字儲存進 DB
     saveMessageTemplate () {
-      // const msg = JSON.stringify(messageTemplate)
+      // const msg = JSON.stringify(this.msgData)
       // do upload
     }
   }
