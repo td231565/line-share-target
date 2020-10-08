218f52e9c527670cf47bf2e496fa1ac8ce44cd7b
Richard <rich_chen@hpicorp.com.tw>
2020-10-08 11:56:54 +0800

    add loading state

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <template>
   <div id="app">
-    <p>{{ isLogin ? 'already login' : 'please login' }}</p>
+    <p>
+      {{ isLogin ? 'already login' : 'please login' }}
+      <span v-if="isLogin">讀取中，請稍後</span>
+    </p>
     <button @click="switchUserLogin">{{ isLogin ? 'logout' : 'login' }}</button>
-    <FlexTemplate v-if="isLogin" />
+    <FlexTemplate class="mt-4" v-if="isLogin" />
   </div>
 </template>
 
@@ -17,13 +20,16 @@ export default {
   },
   data () {
     return {
+      isLoading: false,
       liffId: '1654944538-wRrGzXyX',
       isLogin: false
     }
   },
   methods: {
-    switchUserLogin () {
-      this.isLogin ? liff.logout() : liff.login()
+    async switchUserLogin () {
+      this.isLogin = true
+      await this.isLogin ? liff.logout() : liff.login()
+      this.isLogin = false
     },
     initLiff () {
       liff.init({ liffId: this.liffId }).then(() => {
